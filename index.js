1a02ea5b4e3fab1b6904913cd765402fa23fdcc1
Joseph Baruch <josephbaruch48@gmail.com>
2023-05-24 18:21:47 -0600

    Initialized my disease data app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3002;
+
+app.get('/', (req, res) =>{
+    res.sendFile('index.html', {root: __dirname });
+});
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`); 
+});
