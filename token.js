be85da29780f17bd5122a280e535b9575799819a
Joseph Baruch <josephbaruch48@gmail.com>
2023-06-29 22:20:19 -0600

    Created a callback function scheme for token.js

--- token.js
+++ token.js
@@ -6,32 +6,47 @@
     - Multiple fetch statements for the different types of data that I will display
     - need to figureout how to manipulate the URL so I can get differnet times, data types and locations.
         - this goes into the callbacks problem   
+
+    https://api.meteomatics.com/2023-06-29T21:50:00.000-06:00/t_2m:F/51.5073219,-0.1276474/json?model=mix
  */
 
 // header to be used in token fetch     
-    username='na_baruch'
-    password='K082ZKc6go'
+    username = 'na_baruch'
+    password = 'K082ZKc6go'
+
+    let dateTime =  '2023-06-29T21:50:00.000-06:00' + '/';
+    let statType = 't_2m:F' + '/';
+    let place = '51.5073219,-0.1276474' + '/';
+    let dataType = 'json?'; //json?model=mix?   ...... json?
+    let URL = 'https://api.meteomatics.com/' + dateTime + statType + place + dataType;
     let headers = new Headers();
     headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 
 // ------ fetch token ------------
-    fetch('https://login.meteomatics.com/api/v1/token', {
-        method: 'GET', headers: headers
-
-        }).then(function (resp) {
-            return resp.json();
-        }).then(function (data) {
-            var token = data.access_token;
-            console.log('token', token);
-        }).catch(function (err) {
-            console.log('something went wrong', err);
-        });
-
+    function getToken(){
+        fetch('https://login.meteomatics.com/api/v1/token', {
+            method: 'GET', headers: headers
+            }).then(function (resp) {
+                return resp.json();
+            }).then(function (data) {
+                var token = data.access_token;
+                console.log('token', token);
+                let accessAndToken = 'access_token=' + token;
+                fetchData(accessAndToken);
+            }).catch(function (err) {
+                console.log('something went wrong', err);
+            });
+    }
+    
 // ------ fetch weather data from the meteomatics API --------
-    // this fetch statement will not be this long. 
-    fetch("https://api.meteomatics.com/2023-06-28T12:05:00.000-06:00/t_2m:F/43.7327012,-116.2831349/json?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJ2IjoxLCJ1c2VyIjoibmFfYmFydWNoIiwiaXNzIjoibG9naW4ubWV0ZW9tYXRpY3MuY29tIiwiZXhwIjoxNjg3OTk2MTUzLCJzdWIiOiJhY2Nlc3MifQ.zd_hzowA87WWXu2ixGyBG3KDIubwNDoJEt4s5iiz9AJm92JOQTY2fdxA-3JYBklanqevxXis70-dCh05yK_DhA" )
-        .then(res => {
-            return res.json();
-        }).then( data => {
-            console.log(data.data[0].coordinates[0].dates[0].value); 
-        }).catch(error => console.log(error));
+    function fetchData(theToken){
+        console.log(URL);
+        fetch(URL + theToken)
+            .then(res => {
+                return res.json();
+            }).then( data => {
+                console.log(data.data[0].coordinates[0].dates[0].value); 
+            }).catch(error => console.log(error));
+    }
+    
+    getToken();
