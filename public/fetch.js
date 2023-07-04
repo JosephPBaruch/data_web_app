e27993e8206880b4ac86d2a500e907e4bd9ce754
Joseph Baruch <josephbaruch48@gmail.com>
2023-07-04 16:38:25 -0600

    Started adding data

--- public/fetch.js
+++ public/fetch.js
@@ -16,7 +16,7 @@
 
 // fetchData URL breakdown 
     let dateTime =  '2023-07-04T10:05:00.000-06:00' + '/'; // data and time
-    let statType = 't_2m:F,sunrise:sql' + '/'; // temperature 2 meters above ground in F
+    let statType = 't_2m:F,sunrise:sql,uv:idx' + '/'; // temperature 2 meters above ground in F
     let place = '43.7324936,-116.283308' + '/'; // long, lat
     let dataType = 'json?'; // data type in .json form
     let access = 'access_token=';
@@ -24,7 +24,7 @@
     let URL = 'https://api.meteomatics.com/' + dateTime + statType + place + dataType + access; 
 
 // ------ fetch token ------------
-let temp = fetch('https://login.meteomatics.com/api/v1/token', {
+let data = fetch('https://login.meteomatics.com/api/v1/token', {
     method: 'GET', headers: headers
     }).then(res => {
         return res.json();
@@ -33,11 +33,11 @@ let temp = fetch('https://login.meteomatics.com/api/v1/token', {
             .then(res => {
                 return res.json(); // treat fetch response as a .json format and return to next promis
             }).then( data => {
-                //return data.data[0].coordinates[0].dates[0].value;
+                return data;
             }).catch(error => console.log(error)); // logs error in console if caught 
     }).catch(function (err) {
         console.log('something went wrong', err);
 });
 
 
-// //43.7324936,-116.283308/json?model=mix
+// https://api.meteomatics.com/2023-07-04T10:05:00.000-06:00/t_2m:F,sunrise:sql,uv:idx/43.7324936,-116.283308/json?model=mix
